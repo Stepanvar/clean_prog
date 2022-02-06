abc25435d933c1bff020f0ed55b0ac0c740d4055
stepan_ubuntu <zsteve2911@gmail.com>
2022-02-06 21:15:06 +0300

    my first ai

--- first_ai/src/my_ai.c
+++ first_ai/src/my_ai.c
@@ -1,86 +1,75 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "time.h"
+#include <math.h>
 
 #define _weight 3
 double	w[_weight];
+
 double	ft_a(double var)
 {
+	// normalize result with sigmoid
+	//var = 1 * 1.0 / (1 + pow(2.7318281828, var * -1.0));
 	if (var > 0)
-		return 1;
-	else
-		return 0;
+		return (1);
+	else 
+		return (0);
 }
 
-double	ft_rp(double i[3])
+double	ft_ai(double arr[3])
 {
-	int j = 0;
-	double res = 0;
-	while (j < 3)
+	double	res = 0;
+	int		k = 0;
+	while (k < _weight)
 	{
-	
-		res += i[j] * w[j];
-		j++;
+		res += w[k] * arr[k];
+		k++;
 	}
 	return (res);
 }
 
-void	ft_pr(double a[3])
+void	ft_train(double set[4][3], double expect[4])
 {
-	int i;
+	double res = 0;
+	int	i = 0;
+	int	j = 0;
+	int	l = 0;
 
-	i = 0;
-	while (i < 3)
+	while (i < 50)
 	{
-		printf("%f\n", a[i]);
+		l = 0;
+		while (l < 4)
+		{
+			res = 0;
+			j = 0;
+			res = ft_a(ft_ai(set[l]));
+			while (j < _weight)
+			{
+				w[j] = w[j] + 0.1 * (expect[l] - res) * set[l][j];
+				j++;
+			}
+			l++;
+		}
 		i++;
 	}
-	printf("\n");
 }
 
-int main(void)
+int	main(void)
 {
-	int		i;
-	int		j;
-	double	res;
-	double	a[3][3] = {{0, 0, 1}, {0, 1, 1}, {1, 0, 0}};
-	double	ans[3] = {1, 1, 0};
-
-	i = 0;
-	j = 0;
-	res = 0;
-	srand(time(0));
+	double	mass[4][3] = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}, {1, 0, 1}};
+	double	out[4] = {1, 0, 0, 1};
+	double	work[3] = {1, 0, 0};
+	int		i = 0;
+	double	res = 0;
+	srand(time(NULL));
 	while (i < _weight)
 	{
-		w[i] = rand() * 1.0 / RAND_MAX + 0.000001;
+		w[i] = rand() * 1.0 / RAND_MAX;
 		i++;
 	}
-	i = 0;
-	printf("w old\n");
-	ft_pr(w);
-	while (j < 3)
-	{
-		i = 0;
-		res = (double) ft_rp(a[j]);
-		while (i < 3)
-		{
-			w[i] += (0.1 * (ans[j] - res) * a[j][i]);
-			i++;
-		}
-		printf("%d = %f\n", j, res);
-		ft_pr(w);
-		j++;
-	}
-	//printf("w\n");
-	// while (i < 4)
-	// {
-	// 	w[i] = rand() * 1.0 / RAND_MAX;
-	// 	while (w[i] != 1)
-	// 	{
-	// 		w[i] = w[i] + ((1 - w[i]) * 0.1);
-	// 	}
-	// 	printf("%d", w[i]);
-	// 	i++;
-	// }
+	ft_train(mass, out);
+	res = ft_ai(work);
+	printf("%f\t%f\t%f\n", w[0], w[1], w[2]);
+	printf("%f\n", ft_a(res));
 	return (0);
 }
