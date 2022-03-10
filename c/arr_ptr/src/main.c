f618fc3c5324709ab5f3181b9fe9091e03f4de78
stepanvar <zsteve2911@gmail.com>
2022-03-10 21:51:20 +0300

    structurize knowledge

--- c/arr_ptr/src/main.c
+++ c/arr_ptr/src/main.c
@@ -4,13 +4,15 @@
 int	main(void)
 {
 	int	i = 0;
-//	i = ft_arr_vs_ptr();
+	i = ft_arr_vs_ptr();
 	//theory: ptr will get set of the addresses which yoy can modify
 	//you can change a value of the variable via its adress
 	char	*str;
 	char	d = '@';
 	char	c = ',';
 	char	*str1;
+	char	arr[5];
+
 	str = &d;
 	printf("%s\n", str);
 	*str = c;
