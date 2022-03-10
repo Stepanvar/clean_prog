6d53be83c306750e1db84ad2b40ba081124789bb
stepanvar <zsteve2911@gmail.com>
2022-03-10 15:46:14 +0300

    modify a value you will modify the pointer value

--- c/arr_ptr/src/main.c
+++ c/arr_ptr/src/main.c
@@ -14,10 +14,15 @@ int	main(void)
 	ptr = &(arr[0]);
 	printf("%s\n", arr);
 	printf("%s\n", ptr);
-	printf("arr can be declare as char * and printed with %%s\n");
+	printf("arr as a ptr can be declare as char * and printed with %%s\n");
 	printf("ptr can be assigned a value\n");
-	arr = ptr; //not assignable
+//	arr = ptr; //not assignable
+	printf("arr can't be assignable\n");
 	printf("sizeof arr %lu\n", sizeof(arr));
 	printf("sizeof ptr %lu\n", sizeof(ptr));
+	arr[2] = *ptr;
+	printf("%s", ptr);
+	printf("ptr save an adress of value, so when the value is changed the pounter values is also changed\n");
+	//int adress in arr and get it access
 	return (0);
 }
