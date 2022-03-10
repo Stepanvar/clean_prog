0bb68494e318acbd01e1bc6595cbf9e7ee7d6cf5
stepanvar <zsteve2911@gmail.com>
2022-03-10 17:44:26 +0300

    change value of a variable implicitly

---
+++ c/arr_ptr/src/collection.h
@@ -0,0 +1,4 @@
+#ifndef COLLECTION_H
+# define COLLECTION_H
+int	ft_arr_vs_ptr(void);
+#endif
--- c/arr_ptr/src/main.c
+++ c/arr_ptr/src/main.c
@@ -1,28 +1,21 @@
+#include "collection.h"
 #include "stdio.h"
 
 int	main(void)
 {
-	//declare arr
-	char	arr[5];
-	//declare ptr
-	char	*ptr;
-	arr[0] = 35;
-	arr[1] = 48;
-	arr[2] = 56;
-	arr[3] = 64;
-	arr[4] = 0;
-	ptr = &(arr[0]);
-	printf("%s\n", arr);
-	printf("%s\n", ptr);
-	printf("arr as a ptr can be declare as char * and printed with %%s\n");
-	printf("ptr can be assigned a value\n");
-//	arr = ptr; //not assignable
-	printf("arr can't be assignable\n");
-	printf("sizeof arr %lu\n", sizeof(arr));
-	printf("sizeof ptr %lu\n", sizeof(ptr));
-	arr[2] = *ptr;
-	printf("%s", ptr);
-	printf("ptr save an adress of value, so when the value is changed the pounter values is also changed\n");
-	//int adress in arr and get it access
-	return (0);
+	int	i = 0;
+//	i = ft_arr_vs_ptr();
+	//theory: ptr will get set of the addresses which yoy can modify
+	//you can change a value of the variable via its adress
+	char	*str;
+	char	d = '@';
+	char	c = ',';
+	char	*str1;
+	str = &d;
+	printf("%s\n", str);
+	*str = c;
+	printf("%s\n", str);
+	printf("%c\n", d);
+	printf("You can change the value implicitly when assign a new value to the pointer\n");
+	return (i);
 }
