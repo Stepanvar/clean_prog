d4c1bbd13a93417fd0eb58a33ee0de2fabf1fea0
stepanvar <zsteve2911@gmail.com>
2022-03-11 22:51:32 +0300

    sort arr and ptr info

--- c/arr_ptr/src/main.c
+++ c/arr_ptr/src/main.c
@@ -8,12 +8,9 @@ int	main(void)
 	//i = ft_arr_vs_ptr();
 	//theory: ptr will get set of the addresses which you can modify
 	//you can change a value of the variable via its adress
-	char	*ptr1;
-	char	*ptr2;
-	char	*ptr3;
-	char	*ptr4;
-
-	ptr1 = (char *)malloc(10 * sizeof(char) + 1);
-
+	char	*ptr;
+	char	arr[5];
+	ft_ptr_info(ptr);
+	ft_arr_info(arr);
 	return (i);
 }
