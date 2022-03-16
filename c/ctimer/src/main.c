4649c683d8486736e8597f65e93aeb120246cc31
stepanvar <zsteve2911@gmail.com>
2022-03-16 14:45:46 +0300

    string can be given to the system func

--- c/ctimer/src/main.c
+++ c/ctimer/src/main.c
@@ -1,7 +1,16 @@
 #include "stdlib.h"
+#include "libft.h"
+#include "stdio.h"
+
 
 int	main(void)
 {
-	system("termux-toast -b black -g bottom Time_passed");
+	char	*s;
+	char	*command;
+	command = "termux-toast -b black -g bottom Time_passed";
+	system(command);
+	ft_printf("Enter what will you do\n");
+//	scanf("%s", &s);
+
 	return (0);
 }
