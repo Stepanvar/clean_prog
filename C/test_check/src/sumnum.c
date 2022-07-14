2f95efea9b33f9d03aa5a0aa92ed28ee779ec516
stepanvar_ubuntu <zsteve2911@gmail.com>
2022-07-14 21:13:21 +0300

    make first steps on unit testing

---
+++ C/test_check/src/sumnum.c
@@ -0,0 +1,6 @@
+#include "sum.h"
+
+int	main()
+{
+	return sum(10, 20);
+}
--- C/test_check/test/test_sum.c
+++ C/test_check/test/test_sum.c
@@ -1,13 +1,19 @@
 #include <check.h>
 
-#include "sum.h"
-
+#include "../src/sum.h"
+#
 START_TEST(sum2test)
 {
 	fail_unless(sum(3, 3) == 6);
+    fail_unless(sum(40, 20) == 60);
+    ck_assert_int_eq(sum(40, 20), 60);
 }
 END_TEST
-
+START_TEST(sum1test)
+{
+    fail_unless(sum(50, 100) == 150);
+}
+END_TEST
 int main(void)
 {
     Suite *s1 = suite_create("Core");
@@ -17,10 +23,11 @@ int main(void)
 
     suite_add_tcase(s1, tc1_1);
     tcase_add_test(tc1_1, sum2test);
+    tcase_add_test(tc1_1, sum1test);
 
     srunner_run_all(sr, CK_ENV);
     nf = srunner_ntests_failed(sr);
     srunner_free(sr);
 
-    return nf == 0 ? 0 : 1;
+    return nf == 0 ? 0 : nf;
 }
